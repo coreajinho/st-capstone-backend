4c598c8b45dc456dbe0cd20f1ddeeffc9dd48c78
coreajinho <coreajinho1@naver.com>
2025-09-21 02:52:11 +0900

    doc/프로젝트 초기 세팅

---
+++ st-capstone-backend/src/test/java/org/example/stcapstonebackend/StCapstoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stcapstonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StCapstoneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
